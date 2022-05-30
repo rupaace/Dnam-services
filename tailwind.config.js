0e51ddce4339fe5c41f351beab3eaee9f632fa59
rupaace <rupaace42@gmail.com>
2022-05-30 22:16:50 +0545

    initial  commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      Roboto:["Roboto", "sans-serif"],
+    },
+    colors: {
+      'yellow': '#F8CB2E',
+      'red': '#f8373f',
+      'var-blue': '#006E7F',
+      'orange':'#EE5007',
+    },
+    extend: {
+      textColor: ['visited'],
+
+    },
+  },
+  plugins: [],
+}
